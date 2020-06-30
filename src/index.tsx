b58da37577e1e8a1de55452c6da482e16d813d96
yuji-okayama <y-okayama@tribeck.jp>
2020-06-30 13:01:29 +0900

    buildのpath変更

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
-import './index.css';
 import store from './store';
+import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
